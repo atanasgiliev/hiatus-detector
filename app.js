fd279df53e38d9823cd0cfe808679d84b82d6a43
Atanas Iliev <atanasgiliev03@gmail.com>
2026-01-25 17:52:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -177,6 +177,25 @@ write_outputs(annotated, occ, Path("/out.html"), Path("/out.csv"))
 }
 
 /* ----------------------------
+   FILE DOWNLOADS
+----------------------------- */
+
+function downloadFile(filename, content, mime) {
+    const blob = new Blob([content], { type: mime });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+}
+
+
+/* ----------------------------
    RUN BUTTON HANDLER
 ----------------------------- */
 
